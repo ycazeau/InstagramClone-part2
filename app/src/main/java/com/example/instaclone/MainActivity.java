7a5e3e967749fb2fe5ead0d0246230d677b4c2ac
ycazeau <ycazeau23@gmail.com>
2020-05-31 18:42:34 -0400

    Bottom Navigation View

--- app/src/main/java/com/example/instaclone/MainActivity.java
+++ app/src/main/java/com/example/instaclone/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.instaclone;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,12 +14,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCaptureImage;
     private ImageView ivPostImage;
     private Button btnSubmit;
+    private BottomNavigationView bottomNavigationView;
     private File photoFile;
     public String photoFileName = "photo.jpg";
 
@@ -49,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage = findViewById(R.id.btnCaptureImage);
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
 
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +84,26 @@ public class MainActivity extends AppCompatActivity {
                 savePost(description, currentUser, photoFile);
             }
         });
+
+          bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+              @Override
+              public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                  Fragment fragment;
+                  switch (menuItem.getItemId()) {
+                      case R.id.action_home:
+                          Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                          break;
+                      case R.id.action_compose:
+                          Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                          break;
+                      case R.id.action_profile:
+                          Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                      default:
+                          break;
+                  }
+                  return true;
+              }
+          });
 
     }
 
