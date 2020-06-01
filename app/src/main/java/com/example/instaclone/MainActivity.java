4f88d2f297d6f6b4be050660e0a328bae21365e5
ycazeau <ycazeau23@gmail.com>
2020-06-01 12:48:36 -0400

    Fragment Manager

--- app/src/main/java/com/example/instaclone/MainActivity.java
+++ app/src/main/java/com/example/instaclone/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -21,6 +22,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.instaclone.fragments.ComposeFragment;
+import com.example.instaclone.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -36,13 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
       @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
 
@@ -52,20 +55,27 @@ public class MainActivity extends AppCompatActivity {
                   Fragment fragment;
                   switch (menuItem.getItemId()) {
                       case R.id.action_home:
+                          // TODO: update fragment
                           Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                          fragment = new PostsFragment();
                           break;
                       case R.id.action_compose:
                           Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                          fragment = new ComposeFragment();
                           break;
                       case R.id.action_profile:
-                          Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
                       default:
+                          // TODO: update fragment
+                          Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                          fragment = new ComposeFragment();
                           break;
                   }
+                  fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                   return true;
               }
           });
-
+          // Set default selection
+          bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
 }
